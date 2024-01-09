62691fbec46d6281354fd4485fcea391e9c158f1
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-01-10 01:21:42 +0530

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -28,13 +28,13 @@ up = { "ytdl": False,'Total':0}
 
 links=[
   #  "https://xhamster.com/creators/leathers-kittens"
-    "https://xhamster.com/creators/shraboni"
+  #  "https://xhamster.com/creators/shraboni"
  # "https://www.pornhub.com/playlist/80286461"
    # "https://www.pornhub.com/playlist/301331341"
    #"https://www.pornhub.com/playlist/129048362",
     #"https://www.pornhub.com/playlist/85569291",
     #"https://www.pornhub.com/playlist/115901811"
-     #"https://www.pornhub.com/playlist/275691841",
+     "https://www.pornhub.com/playlist/275691841"
       #"https://www.pornhub.com/playlist/293168491",
        #"https://www.pornhub.com/playlist/263313231"
        
